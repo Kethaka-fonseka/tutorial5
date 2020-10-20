2953258b905532997716797d704b11fa4d062fcd
Kethaka-Fonseka <kethaka.fsk@gmail.com>
2020-10-20 18:16:47 +0530

    initial commit

---
+++ app/src/main/java/com/example/datahandling/Database/UsersMaster.java
@@ -0,0 +1,15 @@
+package com.example.datahandling.Database;
+
+import android.provider.BaseColumns;
+
+public class UsersMaster {
+
+    public UsersMaster() {
+    }
+
+    public static class Users implements BaseColumns{
+        public static  final String  TABLE_NAME="users";
+        public static  final String  COLUMN_NAME_USERNAME="username";
+        public static  final String  COLUMN_NAME_PASSWORD="password";
+    }
+}
